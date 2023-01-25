2e1c66450d748cf27a5700f3e80429a2637cbc79
sajjadalidev <sajjadali440990@gmail.com>
2023-01-25 12:36:10 +0500

    nested table added

--- src/Components/searchConsoleTable.js
+++ src/Components/searchConsoleTable.js
@@ -10,6 +10,54 @@ const itemRender = (_, type, originalElement) => {
   return originalElement;
 };
 
+const expandedRowRender = () => {
+  const expanded_columns = [
+    {
+      title: "KWs",
+      dataIndex: "kws",
+    },
+    {
+      title: "Clicks",
+      dataIndex: "clicks",
+    },
+    {
+      title: "Impression",
+      dataIndex: "impression",
+    },
+    {
+      title: "% Clicks of Cluster",
+      dataIndex: "click_of_cluster",
+    },
+    {
+      title: "% Impression of Cluster",
+      dataIndex: "impression_of_cluster",
+    },
+    {
+      title: "Position",
+      dataIndex: "position",
+    },
+  ];
+  const expanded_data = [];
+  for (let i = 0; i < 3; ++i) {
+    expanded_data.push({
+      key: i.toString(),
+      kws: "bakken",
+      clicks: "34",
+      click_of_cluster: "56%",
+      impression: "56",
+      impression_of_cluster: "6%",
+      position: "34",
+    });
+  }
+  return (
+    <Table
+      columns={expanded_columns}
+      dataSource={expanded_data}
+      pagination={false}
+    />
+  );
+};
+
 const columns = [
   {
     title: "Groups",
@@ -62,100 +110,26 @@ const data = [
     detail: "View KWS(12)",
     description:
       "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
-    children: [
-      {
-        key: "56",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "34",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "349",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-    ],
   },
   {
     key: "2",
     groups: "John Brown",
-    total_clicks: 8,
-    total_impressions: 6,
-    position: 7,
-    detail: "View KWS(19)",
-    children: [
-      {
-        key: "56",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "34",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "349",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-    ],
+    total_clicks: 90,
+    total_impressions: 70,
+    position: 78,
+    detail: "View KWS(12)",
+    description:
+      "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
   },
   {
     key: "3",
-    groups: "Not Expandable",
-    total_clicks: 18,
-    total_impressions: 20,
-    position: 40,
-    detail: "View KWS(2)",
-    children: [
-      {
-        key: "56",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "34",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-      {
-        key: "349",
-        groups: "John Brown",
-        total_clicks: 90,
-        total_impressions: 70,
-        position: 78,
-        detail: "View KWS(12)",
-      },
-    ],
+    groups: "John Brown",
+    total_clicks: 90,
+    total_impressions: 70,
+    position: 78,
+    detail: "View KWS(12)",
+    description:
+      "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
   },
 ];
 const onChange = (pagination, filters, sorter, extra) => {
@@ -172,6 +146,9 @@ export const SearchConsoleTable = () => (
       dataSource={data}
       onChange={onChange}
       pagination={false}
+      expandable={{
+        expandedRowRender,
+      }}
     />
     <Pagination
       total={500}
